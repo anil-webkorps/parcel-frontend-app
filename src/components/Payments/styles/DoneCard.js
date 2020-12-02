76bdd21030d2cf6a2922c4b6c919cf08ccf613bf
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-12-03 01:57:15 +0530

    cleanup

--- src/components/Payments/styles/DoneCard.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/macro";
-
-export default styled.div`
-  padding: 30px 36px;
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
--- src/components/Payments/styles/index.js
+++ src/components/Payments/styles/index.js
@@ -1,14 +1,6 @@
 export { default as Container } from "./Container";
-export { default as ChooseDepartment } from "./ChooseDepartment";
-export { default as StepsCard } from "./StepsCard";
-export { default as PayrollCard } from "./PayrollCard";
-export { default as DoneCard } from "./DoneCard";
 export { default as Text } from "./Text";
-export { default as Summary } from "./Summary";
 export { default as Table } from "./Table";
-export { default as Departments } from "./Departments";
-export { default as ActionItem } from "./ActionItem";
-export { default as AllEmployees } from "./AllEmployees";
 export { default as PaymentSummary } from "./PaymentSummary";
 export { default as TokenBalance } from "./TokenBalance";
 export { default as Heading } from "./Heading";
