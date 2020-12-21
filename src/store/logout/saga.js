28dd88887679f998cea1f954dbc7f40ba5695c2d
tarun1475 <tarunkumargupta14@gmail.com>
2020-12-21 14:18:51 +0530

    logout hot fix

--- src/store/logout/saga.js
+++ src/store/logout/saga.js
@@ -1,5 +1,5 @@
-import { put, takeEvery } from "redux-saga/effects";
-import { push } from "connected-react-router";
+import { takeEvery } from "redux-saga/effects";
+// import { push } from "connected-react-router";
 import { LOGOUT_USER } from "./action-types";
 
 export function* logout() {
@@ -8,7 +8,8 @@ export function* logout() {
 
 function* invalidateSession() {
   yield localStorage.removeItem("token");
-  yield put(push("/"));
+  window.location = "/";
+  // yield put(push("/"));
 }
 
 export default function* watchLogout() {
