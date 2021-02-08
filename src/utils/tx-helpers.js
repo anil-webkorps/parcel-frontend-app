0b5a627ef20c68df94df95a08fb008bb337accfe
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2021-02-08 16:48:53 +0530

    cleanup

--- src/utils/tx-helpers.js
+++ src/utils/tx-helpers.js
@@ -1,4 +1,4 @@
-import { BigNumber } from "@ethersproject/bignumber";
+// import { BigNumber } from "@ethersproject/bignumber";
 import { parseUnits } from "@ethersproject/units";
 // Hex helpers
 export function joinHexData(hexData) {
